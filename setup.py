3b8360ad2923624c1620715e73d3fc324d663170
chialiang <chialiang_8@163.com>
2015-10-10 20:02:06 +0800

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+setuptools.setup(
+    setup_requires=['pbr'], pbr=True)
